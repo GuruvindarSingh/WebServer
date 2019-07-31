97fd0e73a890648e6ba68766fc67e438015588a4
Guruvindar Singh <749083@student.tor.loc>
2019-07-31 14:57:31 -0400

    commit

---
+++ purenodeserver.js
@@ -0,0 +1,9 @@
+const http = require('http');
+function handler(req, res){
+res.writeHead(200, {'Content-type': 'text/plain'});
+res.write('Hello, I am a Webserver!');
+res.end();
+
+}
+const server = http.createServer(handler);
+server.listen(3000);
